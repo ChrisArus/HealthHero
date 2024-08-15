8e0b9a785243259cd2bc36f9cb8bf8e48d1a7190
ChrisArus <161660141+ChrisArus@users.noreply.github.com>
2024-08-15 19:55:30 -0400

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+// frontend/src/App.js
+import './styles.css';
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Outlet />
+      </div>
+    </ApolloProvider>
+
+);
+}
+export default App;
